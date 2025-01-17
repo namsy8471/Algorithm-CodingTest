c2dcd0d7c18c147958113720fdc723f38ab295ea
Siyoung Nam <31647755+namsy8471@users.noreply.github.com>
2025-01-17 23:13:32 +0700

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//MenOfPassion(A[], n) {
+//    sum < -0;
+//    for i < -1 to n
+//        for j < -1 to n
+//            for k < -1 to n
+//                sum < -sum + A[i] × A[j] × A[k]; # 코드1
+//                return sum;
+//}
+
+int main()
+{
+	long long cnt;
+	cin >> cnt;
+
+	cout << cnt * cnt * cnt << endl;
+	cout << 3 << endl;
+
+	return 0;
+}
