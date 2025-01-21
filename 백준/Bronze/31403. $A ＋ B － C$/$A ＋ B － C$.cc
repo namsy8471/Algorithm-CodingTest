eb9c32a9568dfe1ecdf15b18fbcec9d096fffe22
Siyoung Nam <31647755+namsy8471@users.noreply.github.com>
2025-01-22 03:04:22 +0700

    [Bronze IV] Title: $A + B - C$, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    cout << a + b - c << endl;
+    cout << stoi(to_string(a) + to_string(b)) - c << endl;
+
+    return 0;
+}
