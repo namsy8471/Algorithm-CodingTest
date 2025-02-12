68d5a690675de72e409b6fa4cafe33a1a4de3bba
Siyoung Nam <31647755+namsy8471@users.noreply.github.com>
2025-02-12 18:05:21 +0400

    [Silver IV] Title: 제로, Time: 24 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+   
+    int n;
+
+    cin >> n;
+
+    stack<int> s;
+
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+
+        if (num == 0 && !s.empty()) s.pop();
+        else s.push(num);
+    }
+
+    int ret = 0;
+
+    while (!s.empty())
+    {
+        ret += s.top();
+        s.pop();
+    }
+
+    cout << ret;
+
+    return 0;
+}
