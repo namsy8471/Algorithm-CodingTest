60991da751a893a53078e6f0d199119dee474509
Siyoung Nam <31647755+namsy8471@users.noreply.github.com>
2025-02-16 14:09:28 +0400

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> makeDP(int n)
+{
+    vector<int> dp(n + 1);
+
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for (int i = 3; i < n + 1; i++)
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+         
+    return dp;
+}
+
+int main() {
+   
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+
+    cin >> n;
+
+    vector<int> dp = makeDP(n);
+
+    cout << dp[n];
+
+    return 0;
+}
+
+
