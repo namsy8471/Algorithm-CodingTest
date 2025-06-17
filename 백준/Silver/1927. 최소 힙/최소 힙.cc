22a56a1e7ccc2af2b397e259b571b327aa918cb3
Siyoung Nam <31647755+namsy8471@users.noreply.github.com>
2025-06-17 16:29:41 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,36 @@
+#include <iostream>  
+#include <queue>
+
+using namespace std;
+
+int main() {  
+
+    ios::sync_with_stdio(false);  
+    cin.tie(nullptr);  
+
+    int cnt;
+
+    cin >> cnt;
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    while (cnt--)
+    {
+        int cmd;
+        cin >> cmd;
+
+        if (cmd == 0) {
+            if (pq.empty()) cout << 0 << '\n';
+            else {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        }
+
+        else {
+            pq.push(cmd);
+        }
+    }
+
+    return 0;  
+}
