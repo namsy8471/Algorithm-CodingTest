e2f0da55cebef599b90a61f3f7a2bbc207943c87
Siyoung Nam <31647755+namsy8471@users.noreply.github.com>
2025-06-19 15:08:50 +0900

    [Silver II] Title: 나무 자르기, Time: 164 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -0,0 +1,49 @@
+#include <iostream>  
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {  
+
+    ios::sync_with_stdio(false);  
+    cin.tie(nullptr);  
+
+    int numOfTree, len;
+
+    cin >> numOfTree >> len;
+
+    vector<int> v(numOfTree, 0);
+    
+    for (auto& iter : v)
+        cin >> iter;
+
+    sort(v.begin(), v.end());
+
+    long long low = 0;
+    long long high = v.back();
+    
+    long long sum = 0;
+    int ret = 0;
+
+    while (low <= high)
+    {
+        long long mid = low + (high - low) / 2;
+        sum = 0;
+
+        for (int i = 0; i < v.size(); i++)
+            sum += v[i] - mid > 0 ? v[i] - mid : 0;
+
+        if (sum >= len)
+        {
+            ret = mid;
+            low = mid + 1;
+        }
+        else if (sum < len)
+            high = mid - 1;
+    }
+    
+    cout << ret;
+
+    return 0;  
+}
