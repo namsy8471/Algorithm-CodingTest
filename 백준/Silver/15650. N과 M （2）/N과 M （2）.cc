87451c88b53d42ce401a143063442a8a471e5c43
Siyoung Nam <31647755+namsy8471@users.noreply.github.com>
2025-07-08 13:38:38 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+
+vector<int> result;
+
+void backtrack(int start)
+{
+    if (result.size() == M)
+    {
+        for (int i = 0; i < M; i++)
+        {
+            cout << result[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = start; i <= N; i++)
+    {
+        result.push_back(i);
+        backtrack(i + 1);
+        result.pop_back();
+    }
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+   
+    cin >> N >> M;
+
+    backtrack(1);
+
+    return 0;  
+}
